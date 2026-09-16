3fec49f08fdb6e8fd98835e3aeecfc0893edfd47
sanchana <dilsisanchana2004@gmail.com>
2026-09-16 21:51:59 +0530

    Add Hospital specialty and Ward Lookup data

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#define maxPatients 100
+#define numOfSpecialties 4
+#define numOfWards 4
+#define maxBeds 20
+
+//Doctor Specialties Lookup data
+const char *specialtyNames[numOfSpecialties]={"General Practice(OPD)",
+"Paediatrics","Cardiology","Neurology"};
+
+const double specialtyConsultationFees[numOfSpecialties]={1500.00,
+2500.00,4500.00,5000.00};
+
+const int consultationTime[numOfSpecialties]={15,20,30,30};
+
+const int dailyPatientCapacity[numOfSpecialties]={30,20,12,10};
+
+//Hospital Ward Lookup data
+
+const char *wardNames[numOfWards]={"General Ward","Paediatric Ward",
+"Surgical Ward","ICU (Intensive Care Unit)"};
+
+const double dailyBedRates[numOfWards]={3000.00,6000.00,12000.00,
+25000.00};
+
+const int wardBedCapacities[numOfWards]={20,10,10,5};
+
 
 int main()
 {
